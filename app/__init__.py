de19b04b1559df24f3fa8783e2d04ccabd61f538
rubbyhorn <80526646+rubbyhorn@users.noreply.github.com>
2021-04-01 14:31:44 +0300

    index from 'tmp' to /

--- app/__init__.py
+++ app/__init__.py
@@ -2,10 +2,10 @@ from flask import Flask, render_template, send_from_directory
 app = Flask(__name__)
 
 
-@app.route('/tmp')
+@app.route('/')
 def tmp_view():
     app.jinja_env.line_statement_prefix = '#'
-    return render_template('tmp.html')
+    return render_template('index.html')
 
 
 @app.route('/favicon.ico')
